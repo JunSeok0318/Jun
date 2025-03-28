374b3525d309c38dbba4b0cb45bda18772df1d34
ghson <ghson@ocube.co.kr>
2025-03-28 12:05:46 +0900

    뭐... 커밋밋

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ if (document.readyState === "loading") {
 
 function loadEvent() {
   const contentsDiv = document.getElementById("contents");
-  fetch("./link.txt").then(response => response.text()).then(text => {
-    const links = text.split("\n")
-    links.forEach(link => {
-      link = link.trim();
-      if (link){
-        const wrapper = document.createElement('div');
-        wrapper.classList.add('video-wrapper')
-        const iframe = document.createElement('iframe');
-        iframe.src = link;
-        iframe.setAttribute("allowfullscreen", true)
-  
-        wrapper.appendChild(iframe);
-        contentsDiv.appendChild(wrapper);
-      }
-    })
-  });
+  fetch("./link.txt")
+    .then((response) => response.text())
+    .then((text) => {
+      const links = text.split("\n");
+      links.forEach((link) => {
+        link = link.trim();
+        if (link) {
+          const wrapper = document.createElement("div");
+          wrapper.classList.add("video-wrapper");
+          const iframe = document.createElement("iframe");
+          iframe.src = link;
+          iframe.setAttribute("allowfullscreen", true);
+
+          wrapper.appendChild(iframe);
+          contentsDiv.appendChild(wrapper);
+        }
+      });
+    });
+
+  const contentsDiv2 = document.getElementById("contents2");
+  fetch("./link2.txt")
+    .then((response) => response.text())
+    .then((text) => {
+      const links = text.split("\n");
+      links.forEach((link) => {
+        link = link.trim();
+        if (link) {
+          const wrapper = document.createElement("div");
+          wrapper.classList.add("video-wrapper");
+          const iframe = document.createElement("iframe");
+          iframe.src = link;
+          iframe.setAttribute("allowfullscreen", true);
+
+          wrapper.appendChild(iframe);
+          contentsDiv2.appendChild(wrapper);
+        }
+      });
+    });
 }
